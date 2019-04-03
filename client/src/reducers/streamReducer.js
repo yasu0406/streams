926cf8480a3e3c6cc5b2984af6519ea9ed9ed0cf
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-04-03 16:35:18 -0700

    Added FETCH_STREAMS

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,5 +1,7 @@
+import _ from "lodash";
 import {
     CREATE_STREAM,
+    FETCH_STREAMS,
     FETCH_STREAM,
     DELETE_STREAM,
     EDIT_STREAM,
@@ -7,6 +9,8 @@ import {
 
 export default (state = {}, action) => {
     switch (action.type) {
+        case FETCH_STREAMS:
+            return { ...state, ..._.mapKeys(action.payload, 'id') };
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload};
         case CREATE_STREAM:
