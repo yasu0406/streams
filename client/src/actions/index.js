b39fe3a0867ac5b949a5584f44710461e02a9866
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-04-05 16:18:54 -0700

    Refactor editStream function and deleteStream

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -38,19 +38,21 @@ export const fetchStreams = () => async dispatch => {
 };
 
 export const fetchStream = (id) => async dispatch => {
-    const response = await streams.get(`.streams/${id}`);
+    const response = await streams.get(`/streams/${id}`);
 
     dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-    const response = await streams.deleteData(`streams/${id}, formValues`);
+    const response = await streams.patch(`streams/${id}`, formValues);
 
     dispatch({ type: EDIT_STREAM, payload: response.data });
+    history.push('/')
 };
 
 export const deleteStream = (id) => async dispatch => {
-    await streams.delete(`streams/${id}`);
+    await streams.delete(`/streams/${id}`);
 
     dispatch({ type: DELETE_STREAM, payload: id });
+    history.push('/');
 };
